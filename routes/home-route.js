e970ef5b82e209d19c5edcd912feb14154075da4
rlazar25 <119755392+rlazar25@users.noreply.github.com>
2024-06-03 20:48:13 +0200

    Created home controller and connected to home route

--- routes/home-route.js
+++ routes/home-route.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const HomeController = require('../controllers/HomeController');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello world');
-});
+
+router.get('/', HomeController.index);
 
 module.exports = router;
