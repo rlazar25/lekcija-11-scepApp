69db521f9c061d95764e4b3a01f7cd38e9638079
rlazar25 <119755392+rlazar25@users.noreply.github.com>
2024-06-03 20:12:58 +0200

    Added use methods for other folders

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist'))
+
+
+
 app.listen(process.env.PORT, ()=>{
     console.log('Server started on ' + process.env.PORT);
 })
