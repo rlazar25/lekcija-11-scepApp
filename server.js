b4ad18a18be053a6250e6b4bfdeb60123e97a5ed
rlazar25 <119755392+rlazar25@users.noreply.github.com>
2024-06-03 20:38:59 +0200

    created and connected home-route

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/node_modules/bootstrap/dist'))
 
-
+app.use('/', require('./routes'));
 
 app.listen(process.env.PORT, ()=>{
     console.log('Server started on ' + process.env.PORT);
