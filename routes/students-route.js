02cfd58d09c6275fabfcd278260fd7408a519ffa
rlazar25 <119755392+rlazar25@users.noreply.github.com>
2024-06-04 18:03:45 +0200

    Organized routes for students and the student pages

--- routes/students-route.js
+++ routes/students-route.js
@@ -2,6 +2,12 @@ const express = require('express');
 const StudentsController = require('../controllers/StudentsController');
 const router = express.Router();
 
-router.get('/', StudentsController.index)
+router.get('/', StudentsController.index);
+router.get('/create', StudentsController.create);
+
+router.get('/show/:student_id', StudentsController.show);
+
+// POST
+router.post('/create', StudentsController.store);
 
 module.exports = router;
